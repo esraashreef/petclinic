8f23c668a422615f752b6490455ee70d56756877
esraashreef <58656714+esraashreef@users.noreply.github.com>
2021-08-25 20:17:14 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,49 @@
+
+
+const express = require("express")
+const mongoose = require('mongoose')
+
+const Article = require('./models/article')
+
+const articlerouter=require('./routes/articles')
+
+const methodoverride = require ('method-override')
+const app = express()
+
+
+
+
+
+mongoose.connect('mongodb://localhost/blog',{ useNewUrlParser: true,useUnifiedTopology: true, useCreateIndex:true})
+
+
+
+app.set( 'view engine','ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use (methodoverride('_method'))
+
+
+
+
+const port = 5000;
+app.use(express.static('public'))
+
+app.get('/', async(req,res) =>{
+  res.render('index')
+});
+
+
+app.get('/resort', async(req,res) =>{
+ res.render('resort')
+});
+
+app.get('/wellness', async(req,res) =>{
+    res.render('wellness')
+});
+
+app.use('/articles',articlerouter)
+const server = app.listen(port);
+
+
+
+
